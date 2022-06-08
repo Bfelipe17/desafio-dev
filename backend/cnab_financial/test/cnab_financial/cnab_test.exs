d3b69742f61bc5708bbcaa0e194c6ff227c4bc36
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-06-08 16:20:22 -0300

    test: CNAB Changeset

---
+++ backend/cnab_financial/test/cnab_financial/cnab_test.exs
@@ -0,0 +1,37 @@
+defmodule CnabFinancial.CNABTest do
+  use CnabFinancial.DataCase, async: true
+
+  alias Ecto.Changeset
+  alias CnabFinancial.CNAB
+
+  describe "changeset/2" do
+    test "when all params are valid, returns a valid changeset" do
+      params = %{
+        "card" => "6777****1313",
+        "cpf" => "84515254073",
+        "date" => ~D[2019-03-01],
+        "hour" => "172712",
+        "store_name" => "MERCADO DA AVENIDA",
+        "store_owner" => "MARCOS PEREIRA",
+        "type" => "3",
+        "value" => "0000019200"
+      }
+
+      response = CNAB.changeset(params)
+
+      assert %Changeset{valid?: true} = response
+    end
+
+    test "when there is an invalid or missing field, returns an error" do
+      params = %{
+        "card" => "6777****1313",
+        "cpf" => "84515254073",
+        "date" => ~D[2019-03-01]
+      }
+
+      response = CNAB.changeset(params)
+
+      assert %Changeset{valid?: false} = response
+    end
+  end
+end
