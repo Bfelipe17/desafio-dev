47b1a088445ee73485475c9694e92ec327491a4b
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-06-10 14:36:07 -0300

    feat: creating user login

--- frontend/cnab/src/pages/Login/index.tsx
+++ frontend/cnab/src/pages/Login/index.tsx
@@ -1,3 +1,43 @@
+import { useState } from "react";
+import { api } from "../../services/api";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 export function Login() {
-  return <p>Login</p>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // eslint-disable-next-line no-restricted-globals
+    event?.preventDefault();
+    api.post('/users/session', {
+      email,
+      password
+    }).then(function (response) {
+      console.log(response);
+    }).catch(function (error) {
+      toast.error(`${error.response.data.message}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    });
+  }
+
+  return (
+    <>
+      <ToastContainer />
+      <form onSubmit={handleLogin}>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button>Logar</button>
+      </form>
+    </>
+
+  )
 }
