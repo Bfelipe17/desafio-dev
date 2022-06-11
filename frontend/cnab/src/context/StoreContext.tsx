5a9a8d51f7389acc4ac88d9df2738bda916a23f4
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-06-11 12:25:23 -0300

    feat: handling get data into store context

--- frontend/cnab/src/context/StoreContext.tsx
+++ frontend/cnab/src/context/StoreContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
+import { useCookies } from "react-cookie";
+import { api } from "../services/api";
 
 interface StoreProviderProps {
   children: ReactNode;
@@ -7,15 +9,29 @@ interface StoreProviderProps {
 type StoreContextData = {
   storeName: string;
   setStoreName: Dispatch<SetStateAction<string>>;
+  getCnabsBy(storeName: string): void;
+  data: any[];
 };
 
 export const StoreContext = createContext({} as StoreContextData)
 
 export function StoreProvider({ children }: StoreProviderProps) {
+  const [cookies, setCookie, removeCookie] = useCookies(['bycoders_test_token']);
   const [storeName, setStoreName] = useState("ALL");
+  const [data, setData] = useState([]);
+  const token = cookies.bycoders_test_token;
+
+  const getCnabsBy = (storeName: string) => {
+    api.get(`/cnabs/list?store_name=${storeName}`, { headers: { 'Authorization': `Bearer ${token}` } })
+      .then(function (response) {
+        setData(response.data.data)
+      }).catch(function (error) {
+        throw new Error(error)
+      })
+  }
 
   return (
-    <StoreContext.Provider value={{ storeName, setStoreName }}>
+    <StoreContext.Provider value={{ storeName, setStoreName, getCnabsBy, data }}>
       {children}
     </StoreContext.Provider>
   )
