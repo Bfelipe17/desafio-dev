562b2a967a0f405dd8cc526d9102915bbe5a530e
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-06-11 13:06:39 -0300

    feat: handle file upload on store context

--- frontend/cnab/src/context/StoreContext.tsx
+++ frontend/cnab/src/context/StoreContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
+import { ChangeEvent, createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { api } from "../services/api";
 
@@ -12,15 +12,18 @@ type StoreContextData = {
   getCnabsBy(storeName: string): void;
   data: any[];
   total: number;
+  fileHandler(e: ChangeEvent<HTMLInputElement>): void;
+  handleFileSubmit(): void;
 };
 
 export const StoreContext = createContext({} as StoreContextData)
 
 export function StoreProvider({ children }: StoreProviderProps) {
-  const [cookies, setCookie, removeCookie] = useCookies(['bycoders_test_token']);
+  const [cookies, setCookie, removeCookie] = useCookies(["bycoders_test_token"]);
   const [storeName, setStoreName] = useState("ALL");
   const [data, setData] = useState([]);
   const [total, setTotal] = useState(0);
+  const [file, setFile] = useState<File>();
   const token = cookies.bycoders_test_token;
 
   useEffect(() => {
@@ -31,7 +34,7 @@ export function StoreProvider({ children }: StoreProviderProps) {
   }, [data])
 
   const getCnabsBy = (storeName: string) => {
-    api.get(`/cnabs/list?store_name=${storeName}`, { headers: { 'Authorization': `Bearer ${token}` } })
+    api.get(`/cnabs/list?store_name=${storeName}`, { headers: { "Authorization": `Bearer ${token}` } })
       .then(function (response) {
         setData(response.data.data)
       }).catch(function (error) {
@@ -39,8 +42,31 @@ export function StoreProvider({ children }: StoreProviderProps) {
       })
   }
 
+  const fileHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.target.files;
+
+    if (!fileList) return;
+
+    setFile(fileList[0]);
+  }
+
+  const handleFileSubmit = () => {
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file, file.name);
+
+      api.post("/cnabs", formData, {
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      }).then(function (response) {
+        getCnabsBy(storeName);
+      })
+    }
+  }
+
   return (
-    <StoreContext.Provider value={{ storeName, setStoreName, getCnabsBy, data, total }}>
+    <StoreContext.Provider value={{ storeName, setStoreName, getCnabsBy, data, total, fileHandler, handleFileSubmit }}>
       {children}
     </StoreContext.Provider>
   )
