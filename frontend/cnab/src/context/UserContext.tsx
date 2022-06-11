e767cbbed404589be338da53c55cfbfb2ece9549
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-06-11 13:05:21 -0300

    feat: checking if the user is authenticated on user context

--- frontend/cnab/src/context/UserContext.tsx
+++ frontend/cnab/src/context/UserContext.tsx
@@ -7,33 +7,25 @@ interface UserProviderProps {
   children: ReactNode;
 }
 
-type UserContextData = {
-  banana: string;
-};
+export const UserContext = createContext({});
 
-export const UserContext = createContext({} as UserContextData)
-
-export function StoreProvider({ children }: UserProviderProps) {
-  const [cookies, setCookie, removeCookie] = useCookies(['bycoders_test_token']);
+export function UserProvider({ children }: UserProviderProps) {
+  const [cookies, setCookie, removeCookie] = useCookies(["bycoders_test_token"]);
   const token = cookies.bycoders_test_token;
   const navigate = useNavigate();
 
-  const banana = "Banana";
-
   useEffect(() => {
-    const token = cookies.bycoders_test_token;
     if (cookies.bycoders_test_token) {
-      api.get("/users/me", { headers: { 'Authorization': `Bearer ${token}` } })
+      api.get("/users/me", { headers: { "Authorization": `Bearer ${token}` } })
         .catch(function (error) {
-          removeCookie('bycoders_test_token')
-          navigate("/login")
+          removeCookie("bycoders_test_token");
+          navigate("/login");
         })
     }
   }, [])
 
-
   return (
-    <UserContext.Provider value={{ banana }}>
+    <UserContext.Provider value={{}} >
       {children}
     </UserContext.Provider>
   )
