b1575856452e783bb31c1d1fe9c9cf90efb27293
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-06-13 10:44:02 -0300

    fix: removing unused import

--- frontend/cnab/src/context/UserContext.tsx
+++ frontend/cnab/src/context/UserContext.tsx
@@ -1,7 +1,6 @@
 import { createContext, ReactNode, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
-import { Logout } from "../components/Logout";
 import { api } from "../services/api";
 
 interface UserProviderProps {
